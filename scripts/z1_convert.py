c258b224791629199aa3e3ff82f2da6648a4ff9c
mkonopacka <konopackamartyna@gmail.com>
2021-11-14 18:33:42 +0100

    (z1) Add ints_to_floats conversion

--- scripts/z1_convert.py
+++ scripts/z1_convert.py
@@ -1,4 +1,5 @@
 from math import ceil, floor, log2
+import numpy as np
 
 def is_power_of_two(n: int) -> bool:
     """Check if number is power of 2"""
@@ -19,16 +20,31 @@ def ints_to_bits(ints: list, k: int) -> str:
     for x in ints:
         bits += format(x, "b").zfill(k)
     return bits
+
+
+def ints_to_floats(ints: np.ndarray, k: int, a:float = 0, b:float = 1) -> np.ndarray:
+    """Convert array of integers from {0, ... k} to floats in range [a,b]"""
+    floats = []
+    for i,x in enumerate(ints):
+        if x > k: 
+            raise ValueError(f"{x} (on position {i}) is not a number from 0, ... {k}")
+        u_0_1 = x/k 
+        floats.append(u_0_1 * (b-a) + a)  
+    return np.asarray(floats)
+
     
 if __name__ == '__main__':
-    print("Good bits ---------------------")
-    bits1 = '0101111101000101'
-    ints1 = bits_to_ints(bits1, 8)
-    bits2 = ints_to_bits(ints1,2)
-    print(bits1, ints1, bits1 == bits2)
-    bits1 == bits2
+    ints = np.array([1, 3, 9, 0, 6, 8, 8, 9])
+    print(f"ints_to_floats: {ints_to_floats(ints, 10, 0.5, 0.8)}")
+    # print("Good bits ---------------------")
+    # bits1 = '0101111101000101'
+    # ints1 = bits_to_ints(bits1, 8)
+    # bits2 = ints_to_bits(ints1,2)
+    # print(bits1, ints1, bits1 == bits2)
+    # bits1 == bits2
+    
     # print("\nBad bits --------------------")
     # bad_bits = '01011111010001011'
     # ints2 = bits_to_ints(bad_bits, 1)
-    ints2 = bits_to_ints(bits1, 30)
-    print(ints2)
+    # ints2 = bits_to_ints(bits1, 30)
+    # print(ints2)
