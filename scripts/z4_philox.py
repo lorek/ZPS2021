7c082eb4f8c5ecf6a8154c26e8d8b8b05c6c1035
vendia0 <92400509+vendia0@users.noreply.github.com>
2021-12-13 21:27:29 +0100

    Update z4_philox.py

--- scripts/z4_philox.py
+++ scripts/z4_philox.py
@@ -1,50 +1,80 @@
 import numpy as np
+
 import time
-import argparse
-import pickle
 from numpy.random import Philox
+import pandas as pd
 
+import argparse
 
-#  python z4_philox_prng.py --output-file philox_numbers.pkl
-
+import pickle
 
 def ParseArguments():
     parser = argparse.ArgumentParser(description="Philox PRNG")
-    parser.add_argument('--n', default="1000", required=False, help='nr of generated numbers (default: %(default)s)')
-    parser.add_argument('--M', default="1024", required=False)
-    parser.add_argument('--s', required=False)
+    parser.add_argument('--n', default="10000", required=False, help='nr of generated numbers (default: %(default)s)')
+    parser.add_argument('--M', default="1024", required=False, help='modulus (default: %(default)s)')
     parser.add_argument('--output-file', default="generated_numbers.pkl", required=False,
                         help='output file (default: %(default)s)')
-
+    parser.add_argument('--seeds', default="", required=False, help='File (.csv) with seeds  (default: %(default)s)')
+    parser.add_argument('--output-dir', default="", required=False,
+                        help='File (.csv) with seeds  (default: %(default)s)')
     args = parser.parse_args()
 
-    return args.n, args.M, args.s, args.output_file
+    return args.n, args.M, args.output_file, args.seeds, args.output_dir
 
 
-n, M, s1, output_file = ParseArguments()
+def my_philox(n, s, M):
+    rng_philox = np.random.default_rng(np.random.Philox(seed=s))
+
+    numbers_philox_01 = rng_philox.random(n)
+    numbers_philox = [int(number * M) for number in numbers_philox_01]
+    return numbers_philox
+
+
+n, M, output_file, seeds_file, output_dir = ParseArguments()
 
 n = int(n)
 M = int(M)
 
-if s1:
-    s = s1
+if seeds_file != "":
+    print("ASDF")
+    df_seeds = pd.read_csv(seeds_file)
+    seeds = df_seeds["seeds"].to_numpy()
 else:
-    s = int(time.time())
+    seeds = np.array([int(time.time())])
 
-rng_philox = np.random.default_rng(np.random.Philox(seed=s))
+if output_dir != "":
+    output_dir += "/"
 
-numbers_philox_01 = rng_philox.random(n)
-numbers_philox = [int(number * M) for number in numbers_philox_01]
+if len(seeds) == 1:
+    generated = my_philox(n, seeds, M)
 
-if output_file == "":
-    print("Wygenerowane liczby: \n", numbers_philox)
+    if output_file == "":
+        print("Wygenerowane liczby: \n", generated)
+    else:
+        data = {'PRNG': "Philox:",
+                'Modulus': M,
+                'n': n,
+                'numbers': generated}
+
+        data_outfile = open(output_file, 'wb+')
+        pickle.dump(data, data_outfile)
+        print("Wygenerowane liczby zapisano w: ", output_file)
+
 else:
-    data = {'PRNG': "Philox",
-            'n': n,
-            'Modulus': M,
-            'numbers': numbers_philox}
+    output_file_list = output_file.split(".")
+    output_file_prefix = "".join(output_file_list[:(len(output_file_list) - 1)])
 
-    data_outfile = open(output_file, 'wb+')
-    pickle.dump(data, data_outfile)
+    for nr, seed in enumerate(seeds):
+        generated = my_philox(n, seed, M)
 
-    print("Wygenerowane liczby zapisano w: ", output_file)
+        output_file = output_dir + output_file_prefix + "_seed_" + str(nr) + ".pkl"
+
+        data = {'PRNG': "Philox:",
+                'Modulus': M,
+                'n': n,
+                'numbers': generated}
+
+        data_outfile = open(output_file, 'wb+')
+        pickle.dump(data, data_outfile)
+
+        print("Saving file nr ", nr, ",\t", output_file)
