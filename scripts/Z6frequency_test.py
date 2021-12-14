5373e941cd04f571fdb1db917ff25befc385a019
ewastebel <93162220+ewastebel@users.noreply.github.com>
2021-12-14 13:12:37 +0100

    Update Z6frequency_test.py

--- scripts/Z6frequency_test.py
+++ scripts/Z6frequency_test.py
@@ -1,22 +1,106 @@
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import time
 from scipy.stats import norm
 import argparse
 import pickle
+import glob, os
 import pandas as pd
 from scipy.stats import chisquare
 
+
+def changeNumbers(numbers_, M=2**32):
+    #max_number = max(numbers_)
+    for i in range(0, len(numbers_)):
+        numbers_[i] = numbers_[i] / M
+    return numbers_
+# e = changeNumbers(numbers)
+# L = 2**8
+def freq_test(numbers, L=2 ** 8):
+    # print(numbers[1:5])
+    a = []
+    for i in range(0, L):
+        a.append(0)
+
+    for i in range(0, L):
+        for j in range(0, len(numbers)):
+            if numbers[j] >= i / L and numbers[j] < (i + 1) / L:
+                a[i] = a[i] + 1
+    # print(a)
+    #print(chisquare(a, ddof=0))
+    p_value = chisquare(a, ddof=0)[1]
+    test_stat = chisquare(a, ddof=0)[0]
+    return p_value #, test_stat
+
+
+'''
 def ParseArguments():
     parser = argparse.ArgumentParser(description="Frequency Test")
     #parser.add_argument('--n', default="100", required=False, help='nr of generated numbers (default: %(default)s)')
     parser.add_argument('--input-file', default="generated_numbers.pkl", required=False, help='output file (default: %(default)s)')
     args = parser.parse_args()
-
     return args.input_file
 
-input_file = ParseArguments()
+input_file = ParseArguments()'''
 
+
+def ParseArguments():
+    parser = argparse.ArgumentParser(description="Project")
+    # parser.add_argument('--n', default="100", required=False, help='nr of generated numbers (default: %(default)s)')
+    parser.add_argument('--input-file', default="generated_numbers.pkl", required=False,
+                        help='input .pkl file (default: %(default)s)')
+    parser.add_argument('--input-dir', default="numbers", required=False,
+                        help='input directory with .pkl files (default: %(default)s)')
+    parser.add_argument('--pval-file', default="p-values.csv", required=False,
+                        help='output file with p-values (default: %(default)s)')
+    args = parser.parse_args()
+
+    return args.input_file, args.input_dir, args.pval_file
+
+input_file, input_dir, pval_file = ParseArguments()
+
+if input_dir == "":  # one .pkl file
+
+    numbers_info = pd.read_pickle(input_file)
+    prng_info = numbers_info['PRNG']
+    n = int(numbers_info['n'])
+    M = numbers_info['Modulus']
+    numbers = numbers_info['numbers']
+    numbers = changeNumbers(numbers, M)
+
+    pval = freq_test(numbers)
+
+    print("pval = ", np.round(pval, 5))
+    pvals = []
+    pvals.append(pval)
+    print("Saving p-value to ", pval_file)
+    df = pd.DataFrame(pvals, columns=["p-value"])
+    df.to_csv(pval_file)
+
+else:  # many .pkl files
+    print("input_dir = ", input_dir)
+    pvals = []
+    file_list = list((glob.glob(input_dir + "\\**.pkl")))
+    file_list.sort()
+    for file_name in file_list:
+        print("Processing file ", file_name, " ...")
+        numbers_info = pd.read_pickle(file_name)
+
+        prng_info = numbers_info['PRNG']
+        n = int(numbers_info['n'])
+        M = numbers_info['Modulus']
+        numbers = numbers_info['numbers']
+        numbers = changeNumbers(numbers, M)
+        #print(numbers[:5])
+        pval = freq_test(numbers)
+        print(pval)
+        pvals.append(pval)
+
+    print("Saving p-values to ", pval_file)
+    df = pd.DataFrame(pvals, columns=["p-value"])
+    df.to_csv(pval_file, index=False)
+
+'''
 numbers_info = pd.read_pickle(input_file)
 
 prng_info = numbers_info['PRNG']
@@ -29,33 +113,7 @@ print("n = ", n)
 print("M = ", M)
 print("5 first numbers: ", numbers[:5])
 
-
 from math import sqrt, erfc
-
-def changeNumbers(numbers):
-    max_number=max(numbers)
-    for i in range(0,len(numbers)):
-        numbers[i]= numbers[i]/M
-    return numbers
-
-e = changeNumbers(numbers)
-L = 2**8
-
-def freq_test(numbers):
-    print(numbers[1:5])
-    a = []
-    for i in range(0, L):
-        a.append(0)
-
-    for i in range(0, L):
-        for j in range(0, len(numbers)):
-            if numbers[j] >= i / L and numbers[j] < (i + 1) / L:
-                a[i] = a[i] + 1
-    print(a)
-    print(chisquare(a,ddof=1))
-    p_value = chisquare(a,ddof=1)[1]
-    test_stat = chisquare(a,ddof=1)[0]
-    return p_value, test_stat
 
 def israndom(p_value):
     if p_value < 0.01:
@@ -64,5 +122,4 @@ def israndom(p_value):
         return True
 
 a = freq_test(numbers)
-print(a)
-
+print(israndom(a[0]))'''
